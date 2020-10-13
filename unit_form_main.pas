a4bb30cf6289fa057ef53c62354270a0c0bf8e0e
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-13 07:39:51 +0700

    Add files via upload

---
+++ unit_form_main.pas
@@ -0,0 +1,43 @@
+unit unit_form_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls,
+  SynEdit;
+
+type
+
+  { TFormMain }
+
+  TFormMain = class(TForm)
+    MainMenuMain: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    StatusBarMain: TStatusBar;
+    SynEditMain: TSynEdit;
+    procedure MenuItem2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+{$R *.lfm}
+
+{ TFormMain }
+
+procedure TFormMain.MenuItem2Click(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+end.
+
