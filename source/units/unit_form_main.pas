3e7d0f0ada7e70accf48d4fa2f100f5fd63b4362
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-13 16:11:57 +0700

    Update unit_form_main.pas

--- source/units/unit_form_main.pas
+++ source/units/unit_form_main.pas
@@ -6,19 +6,42 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls,
-  SynEdit;
+  SynEdit, SynHighlighterPas;
 
 type
 
   { TFormMain }
 
   TFormMain = class(TForm)
+    ImageListMain: TImageList;
     MainMenuMain: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItemSettingsLangPascal: TMenuItem;
+    MenuItemSettingsLang: TMenuItem;
+    MenuItemSettings: TMenuItem;
+    MenuItemFileSave: TMenuItem;
+    MenuItemFileExit: TMenuItem;
+    MenuItemFileSaveAs: TMenuItem;
+    MenuItemFileOpen: TMenuItem;
+    OpenDialogMain: TOpenDialog;
+    PageControlMain: TPageControl;
+    SaveDialogMain: TSaveDialog;
     StatusBarMain: TStatusBar;
     SynEditMain: TSynEdit;
-    procedure MenuItem2Click(Sender: TObject);
+    SynPasSynMain: TSynPasSyn;
+    TabSheet1: TTabSheet;
+    ToolBarMain: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    procedure MenuItem4Click(Sender: TObject);
+    procedure MenuItemFileExitClick(Sender: TObject);
+    procedure MenuItemFileOpenClick(Sender: TObject);
+    procedure MenuItemFileSaveAsClick(Sender: TObject);
+    procedure MenuItemFileSaveClick(Sender: TObject);
+    procedure MenuItemSettingsLangPascalClick(Sender: TObject);
   private
 
   public
@@ -34,9 +57,50 @@ implementation
 
 { TFormMain }
 
-procedure TFormMain.MenuItem2Click(Sender: TObject);
+procedure TFormMain.MenuItemFileExitClick(Sender: TObject);
 begin
   Application.Terminate;
+end;
+
+procedure TFormMain.MenuItem4Click(Sender: TObject);
+begin
+  MessageDlg('About','Anoa-Syntax-Editor',mtInformation,[mbOK],0);
+end;
+
+procedure TFormMain.MenuItemFileOpenClick(Sender: TObject);
+var
+  LFileName : TFileName;
+begin
+  if Self.OpenDialogMain.Execute then
+  begin
+    LFileName := Self.OpenDialogMain.FileName;
+    Self.SynEditMain.Lines.LoadFromFile(LFileName);
+    Self.StatusBarMain.SimpleText := LFileName;
+  end;
+end;
+
+procedure TFormMain.MenuItemFileSaveAsClick(Sender: TObject);
+begin
+  if Self.OpenDialogMain.Execute then
+  begin
+    Self.SynEditMain.Lines.SaveToFile(Self.OpenDialogMain.FileName);
+  end;
+end;
+
+procedure TFormMain.MenuItemFileSaveClick(Sender: TObject);
+var
+  LFileName : TFileName;
+begin
+  LFileName := Self.StatusBarMain.SimpleText;
+  if FileExists(LFileName) then
+  begin
+    Self.SynEditMain.Lines.SaveToFile(LFileName);
+  end;
+end;
+
+procedure TFormMain.MenuItemSettingsLangPascalClick(Sender: TObject);
+begin
+  Self.SynEditMain.Highlighter := Self.SynPasSynMain;
 end;
 
 end.
