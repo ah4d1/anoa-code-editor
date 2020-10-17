92a99b345acad6b8076818be0baf4408f1f03894
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-17 10:55:40 +0700

    Create unit_var.pas

---
+++ dev/units/unit_var.pas
@@ -0,0 +1,31 @@
+unit unit_var;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TUVar = object
+    vCurrentFileName : TFileName;
+    vImageIndexNormalFile : Byte;
+    vImageIndexModifiedFile : Byte;
+    procedure fcInit;
+  end;
+
+var
+  VUVar : TUVar;
+
+implementation
+
+procedure TUVar.fcInit;
+begin
+  Self.vCurrentFileName := '';
+  Self.vImageIndexNormalFile := 3;
+  Self.vImageIndexModifiedFile := 4;
+end;
+
+end.
+
