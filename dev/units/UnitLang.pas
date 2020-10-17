741763b2aff7d6f1439df7d46f41d4ba3fa135a5
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-17 16:35:35 +0700

    Update UnitLang.pas

--- dev/units/UnitLang.pas
+++ dev/units/UnitLang.pas
@@ -6,16 +6,19 @@ interface
 
 uses
   Classes, SysUtils, SynEdit, SynHighlighterHTML, SynHighlighterJava, SynHighlighterPas,
-  SynHighlighterPHP, SynCompletion, UnitVar;
+  SynHighlighterPHP, SynHighlighterPython, SynCompletion, UnitVar;
 
 type
   TULang = object
-    procedure SetHightlighter (AFileName : TFileName; ASynEdit : TSynEdit; ASynHTMLSyn : TSynHTMLSyn;
-      ASynJavaSyn : TSynJavaSyn; ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn);
-    procedure SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit;
+    function SetHightlighter (AFileName : TFileName; ASynEdit : TSynEdit; ASynHTMLSyn :TSynHTMLSyn;
+      ASynJavaSyn : TSynJavaSyn; ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn; ASynPythonSyn : TSynPythonSyn) : string;
+    function SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit;
         ASynHTMLSyn :TSynHTMLSyn; ASynJavaSyn : TSynJavaSyn; ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn;
-        ASynCompletionHTML,ASynCompletionJava,ASynCompletionPas,ASynCompletionPHP : TSynCompletion
-      );
+          ASynPythonSyn : TSynPythonSyn;
+        ASynCompletionHTML,ASynCompletionJava,ASynCompletionPas,ASynCompletionPHP,ASynCompletionPython : TSynCompletion
+      ) : string;
+  private
+    function GetLangTxt (ALang : TASETypeLang) : string;
   end;
 
 var
@@ -23,56 +26,113 @@ var
 
 implementation
 
-procedure TULang.SetHightlighter (AFileName : TFileName; ASynEdit : TSynEdit; ASynHTMLSyn :TSynHTMLSyn;
-  ASynJavaSyn : TSynJavaSyn; ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn);
+function TULang.SetHightlighter (AFileName : TFileName; ASynEdit : TSynEdit; ASynHTMLSyn :TSynHTMLSyn;
+  ASynJavaSyn : TSynJavaSyn; ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn; ASynPythonSyn : TSynPythonSyn) : string;
 var
   LFileExt : string;
+  LLangTxt : string;
 begin
   LFileExt := ExtractFileExt(AFileName);
-  if Pos(LFileExt,ASynHTMLSyn.DefaultFilter) >= 1 then ASynEdit.Highlighter := ASynHTMLSyn
-    else if Pos(LFileExt,ASynJavaSyn.DefaultFilter) >= 1 then ASynEdit.Highlighter := ASynJavaSyn
-    else if Pos(LFileExt,ASynPasSyn.DefaultFilter) >= 1 then ASynEdit.Highlighter := ASynPasSyn
-    else if Pos(LFileExt,ASynPHPSyn.DefaultFilter) >= 1 then ASynEdit.Highlighter := ASynPHPSyn
-  ;
+  if Pos(LFileExt,ASynHTMLSyn.DefaultFilter) >= 1 then
+  begin
+    ASynEdit.Highlighter := ASynHTMLSyn;
+    LLangTxt := Self.GetLangTxt(aseLangHTML);
+  end
+  else if Pos(LFileExt,ASynJavaSyn.DefaultFilter) >= 1 then
+  begin
+    ASynEdit.Highlighter := ASynJavaSyn;
+    LLangTxt := Self.GetLangTxt(aseLangJava);
+  end
+  else if Pos(LFileExt,ASynPasSyn.DefaultFilter) >= 1 then
+  begin
+    ASynEdit.Highlighter := ASynPasSyn;
+    LLangTxt := Self.GetLangTxt(aseLangPas);
+  end
+  else if Pos(LFileExt,ASynPHPSyn.DefaultFilter) >= 1 then
+  begin
+    ASynEdit.Highlighter := ASynPHPSyn;
+    LLangTxt := Self.GetLangTxt(aseLangPHP);
+  end
+  else if Pos(LFileExt,ASynPythonSyn.DefaultFilter) >= 1 then
+  begin
+    ASynEdit.Highlighter := ASynPythonSyn;
+    LLangTxt := Self.GetLangTxt(aseLangPython);
+  end;
+  Result := LLangTxt;
 end;
 
-procedure TULang.SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit;
+function TULang.SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit;
     ASynHTMLSyn :TSynHTMLSyn; ASynJavaSyn : TSynJavaSyn; ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn;
-    ASynCompletionHTML,ASynCompletionJava,ASynCompletionPas,ASynCompletionPHP : TSynCompletion
-  );
+      ASynPythonSyn : TSynPythonSyn;
+    ASynCompletionHTML,ASynCompletionJava,ASynCompletionPas,ASynCompletionPHP,ASynCompletionPython : TSynCompletion
+  ) : string;
+var
+  LLangTxt : string;
 begin
   if ALang = aseLangHTML then
   begin
+    LLangTxt := Self.GetLangTxt(aseLangHTML);
     ASynEdit.Highlighter := ASynHTMLSyn;
     ASynCompletionHTML.Editor := ASynEdit;
     ASynCompletionJava.Editor := nil;
     ASynCompletionPas.Editor := nil;
     ASynCompletionPHP.Editor := nil;
+    ASynCompletionPython.Editor := nil;
   end
   else if ALang = aseLangJava then
   begin
+    LLangTxt := Self.GetLangTxt(aseLangJava);
     ASynEdit.Highlighter := ASynJavaSyn;
     ASynCompletionHTML.Editor := nil;
     ASynCompletionJava.Editor := ASynEdit;
     ASynCompletionPas.Editor := nil;
     ASynCompletionPHP.Editor := nil;
+    ASynCompletionPython.Editor := nil;
   end
   else if ALang = aseLangPas then
   begin
+    LLangTxt := Self.GetLangTxt(aseLangPas);
     ASynEdit.Highlighter := ASynPasSyn;
     ASynCompletionHTML.Editor := nil;
     ASynCompletionJava.Editor := nil;
     ASynCompletionPas.Editor := ASynEdit;
     ASynCompletionPHP.Editor := nil;
+    ASynCompletionPython.Editor := nil;
   end
   else if ALang = aseLangPHP then
   begin
+    LLangTxt := Self.GetLangTxt(aseLangPHP);
     ASynEdit.Highlighter := ASynPHPSyn;
     ASynCompletionHTML.Editor := nil;
     ASynCompletionJava.Editor := nil;
     ASynCompletionPas.Editor := nil;
     ASynCompletionPHP.Editor := ASynEdit;
+    ASynCompletionPython.Editor := nil;
+  end
+  else if ALang = aseLangPython then
+  begin
+    LLangTxt := Self.GetLangTxt(aseLangPython);
+    ASynEdit.Highlighter := ASynPythonSyn;
+    ASynCompletionHTML.Editor := nil;
+    ASynCompletionJava.Editor := nil;
+    ASynCompletionPas.Editor := nil;
+    ASynCompletionPHP.Editor := nil;
+    ASynCompletionPython.Editor := ASynEdit;
   end;
+  Result := LLangTxt;
+end;
+
+function TULang.GetLangTxt (ALang : TASETypeLang) : string;
+var
+  LLangTxt : string;
+begin
+  if ALang = aseLangHTML then LLangTxt := 'HTML'
+    else if ALang = aseLangJava then LLangTxt := 'Java'
+    else if ALang = aseLangPas then LLangTxt := 'Pascal'
+    else if ALang = aseLangPHP then LLangTxt := 'PHP'
+    else if ALang = aseLangPython then LLangTxt := 'Python'
+  ;
+  Result := LLangTxt;
 end;
 
 end.
