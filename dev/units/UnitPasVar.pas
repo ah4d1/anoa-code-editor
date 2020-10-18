8ede4bcbfcfd5406eb6fa8d1335eb34cf7275bbc
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-18 07:09:48 +0700

    Update UnitPasVar.pas

--- dev/units/UnitPasVar.pas
+++ dev/units/UnitPasVar.pas
@@ -14,6 +14,7 @@ type
     vImageIndexNormalFile : Byte;
     vImageIndexModifiedFile : Byte;
     vASETypeLang : TStringList;
+    vReservedWords : array[0..5] of WideString; // see TASETypeLang
     procedure fcInit;
   end;
 
@@ -31,6 +32,51 @@ begin
   Self.vImageIndexNormalFile := 3;
   Self.vImageIndexModifiedFile := 4;
   vASETypeLang := VUTools.FcStringExplode('HTML|Java|JSON|Pascal|PHP|Python','|');
+  {HTML}
+  vReservedWords[0] := '<!DOCTYPE>|<a>|<abbr>|<acronym>|<address>|<applet>|<area>'
+    + '|<article>|<aside>|<audio>|<b>|<base>|<basefont>|<bdi>|<bdo>|<big>|<blockquote>'
+    + '|<body>|<br>|<button>|<canvas>|<caption>|<center>|<cite>|<code>|<col>|<colgroup>'
+    + '|<data>|<datalist>|<dd>|<del>|<details>|<dfn>|<dialog>|<dir>|<div>|<dl>|<dt>|<em>'
+    + '|<embed>|<fieldset>|<figc|ption>|<figure>|<font>|<footer>|<form>|<frame>|<frameset>'
+    + '|<h1>|<h2>|<h3>|<h4>|<h5>|<h6>|<head>|<header>|<hr>|<html>|<i>|<iframe>|<img>|<input>'
+    + '|<ins>|<kbd>|<label>|<|egend>|<li>|<link>|<main>|<map>|<mark>|<meta>|<meter>|<nav>'
+    + '|<noframes>|<noscript>|<object>|<ol>|<optgroup>|<option>|<output>|<p>|<param>|<picture>'
+    + '|<pre>|<progress>|<q>|<rp>|<rt>|<ruby>|<s>|<samp>|<script>|<section>|<select>|<small>'
+    + '|<source>|<span>|<strike>|<strong>|<style>|<sub>|<summary>|<sup>|<svg>|<table>|<tbody>|<td>'
+    + '|<template>|<textarea>|<tfoot>|<th>|<thead>|<time>|<title>|<tr>|<track>|<tt>|<u>|<ul>|<var>|<video>|<wbr>'
+  ;
+  {Java}
+  vReservedWords[1] := 'abstract|assert|boolean|break|byte|case|catch|char|class|continue'
+    + '|const|default|do|double|else|enum|exports|extends|final|finally|float|for|goto|if'
+    + '|implements|import|i|stanceof|int|interface|long|module|native|new|package|private'
+    + '|protected|public|requires|return|short|static|strictfp|super|switch|synchronized|this'
+    + '|throw|throws|transi|nt|try|var|void|volatile|while'
+  ;
+  {JSON}
+  vReservedWords[2] := 'and|as|assert|break|class|continue|def|del|elif|else|except|FALSE'
+    + '|finally|for|from|global|if|import|in|is|lambda|None|nonlocal|not|or|pass|raise'
+    + '|return|TRUE|try|while|with|yield'
+  ;
+  {Pascal}
+  vReservedWords[3] := 'and|array|asm|begin|break|case|const|constructor|continue'
+    + '|destructor|div|do|downto|else|end|FALSE|file|for|function|goto|if'
+    + '|implementation|in|inline|interface|label|mod|nil|not|object|of|on|operator'
+    + '|or|packed|procedure|program|record|repeat|set|shl|shr|string|then|to|TRUE'
+    + '|type|unit|until|uses|var|while|with|xor'
+  ;
+  {PHP}
+  vReservedWords[4] := '__halt_compiler()|abstract|and|array()|as|break|callable|case|catch'
+    + '|class|clone|const|continue|declare|default|die()|do|echo|else|elseif|empty()|enddeclare'
+    + '|endfor|e|dforeach|endif|endswitch|endwhile|eval()|exit()|extends|final|finally|fn'
+    + '|for|foreach|function|global|goto|if|implements|include|include_once|instanceof|insteadof'
+    + '|interface|isset()|list()|namespace|new|or|print|private|protected|public|require|require_once'
+    + '|return|static|switch|throw|trait|try|unset()|use|var|while|xor|yield'
+  ;
+  {Python}
+  vReservedWords[5] := 'and|as|assert|break|class|continue|def|del|elif|else|except|FALSE'
+  + '|finally|for|from|global|if|import|in|is|lambda|None|nonlocal|not|or|pass|raise'
+  + '|return|TRUE|try|while|with|yield'
+;
 end;
 
 end.
