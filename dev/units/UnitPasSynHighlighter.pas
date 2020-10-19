44061e36dbdf3ba6fcf94e5216b6ac70593e32ca
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-19 08:19:32 +0700

    Update UnitPasSynHighlighter.pas

--- dev/units/UnitPasSynHighlighter.pas
+++ dev/units/UnitPasSynHighlighter.pas
@@ -5,10 +5,13 @@ unit UnitPasSynHighlighter;
 interface
 
 uses
-  Classes, SysUtils, SynHighlighterCS, SynHighlighterHTML, SynHighlighterJava, SynHighlighterJSON,
-  SynHighlighterPas, SynHighlighterPHP, SynHighlighterPython;
+  Classes, SysUtils, SynEditHighlighter, SynHighlighterCS, SynHighlighterHTML, SynHighlighterJava,
+  SynHighlighterJSON, SynHighlighterPas, SynHighlighterPHP, SynHighlighterPython;
 
 type
+  TASETypeLang = (aseLangCSharp,aseLangHTML,aseLangJava,aseLangJSON,aseLangPas,aseLangPHP,aseLangPython);
+  {After add TASETypeLang, see also vASETypeLang & vReservedWords at UnitPasVar}
+
   TUSynHighlighter = class
     vCS : TSynCSSyn;
     vHTML : TSynHTMLSyn;
@@ -18,7 +21,9 @@ type
     vPHP : TSynPHPSyn;
     vPython : TSynPythonSyn;
     constructor Create (AOwner : TComponent);
-    function fcDefaultFilter : WideString;
+    function fcSetDefaultFilter : WideString;
+    function fcGetLang (AFileExt : string) : TASETypeLang;
+    function fcGetHighlighter (ALang : TASETypeLang) : TSynCustomHighlighter;
   end;
 
 implementation
@@ -34,7 +39,7 @@ begin
   vPython := TSynPythonSyn.Create(AOwner);
 end;
 
-function TUSynHighlighter.fcDefaultFilter : WideString;
+function TUSynHighlighter.fcSetDefaultFilter : WideString;
 begin
   Result := ''
     + 'All Files (*.*)|*.*'
@@ -46,6 +51,37 @@ begin
     + '|' + Self.vPHP.DefaultFilter
      + '|' + Self.vPython.DefaultFilter
   ;
+end;
+
+function TUSynHighlighter.fcGetLang (AFileExt : string) : TASETypeLang;
+var
+  LLang : TASETypeLang;
+begin
+  if Pos(AFileExt,Self.vCS.DefaultFilter) >= 1 then LLang := aseLangCSharp
+    else if Pos(AFileExt,Self.vHTML.DefaultFilter) >= 1 then LLang := aseLangHTML
+    else if Pos(AFileExt,Self.vJava.DefaultFilter) >= 1 then LLang := aseLangJava
+    else if Pos(AFileExt,Self.vJSON.DefaultFilter) >= 1 then LLang := aseLangJSON
+    else if Pos(AFileExt,Self.vPas.DefaultFilter) >= 1 then LLang := aseLangPas
+    else if Pos(AFileExt,Self.vPHP.DefaultFilter) >= 1 then LLang := aseLangPHP
+    else if Pos(AFileExt,Self.vPython.DefaultFilter) >= 1 then LLang := aseLangPython
+  ;
+  Result := LLang;
+end;
+
+function TUSynHighlighter.fcGetHighlighter (ALang : TASETypeLang) : TSynCustomHighlighter;
+var
+  LResult : TSynCustomHighlighter;
+begin
+  case ALang of
+    aseLangCSharp : LResult := Self.vCS;
+    aseLangHTML   : LResult := Self.vHTML;
+    aseLangJava   : LResult := Self.vJava;
+    aseLangJSON   : LResult := Self.vJSON;
+    aseLangPas    : LResult := Self.vPas;
+    aseLangPHP    : LResult := Self.vPHP;
+    aseLangPython : LResult := Self.vPython;
+  end;
+  Result := LResult;
 end;
 
 end.
