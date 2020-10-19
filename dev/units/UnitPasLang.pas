a2e9e120dafba613ade582e556d69b2d12937222
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-19 08:19:41 +0700

    Update UnitPasLang.pas

--- dev/units/UnitPasLang.pas
+++ dev/units/UnitPasLang.pas
@@ -6,19 +6,12 @@ interface
 
 uses
   Classes, SysUtils, SynEdit, SynHighlighterCS, SynHighlighterHTML, SynHighlighterJava, SynHighlighterJSON,
-  SynHighlighterPas, SynHighlighterPHP, SynHighlighterPython, SynCompletion, UnitPasVar;
+  SynHighlighterPas, SynHighlighterPHP, SynHighlighterPython, SynCompletion, UnitPasVar, UnitPasSynHighlighter;
 
 type
   TULang = object
-    function SetLang (AFileName : TFileName; ASynEdit : TSynEdit;
-      ASynCSSyn : TSynCSSyn; ASynHTMLSyn : TSynHTMLSyn; ASynJavaSyn : TSynJavaSyn; ASynJSONSyn : TSynJSONSyn;
-        ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn; ASynPythonSyn : TSynPythonSyn;
-      ASynCompletion : TSynCompletion) : string; overload;
-    function SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit;
-      ASynCSSyn : TSynCSSyn; ASynHTMLSyn : TSynHTMLSyn; ASynJavaSyn : TSynJavaSyn; ASynJSONSyn : TSynJSONSyn;
-        ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn; ASynPythonSyn : TSynPythonSyn;
-      ASynCompletion : TSynCompletion
-      ) : string;  overload;
+    function SetLang (AFileName : TFileName; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string; overload;
+    function SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string; overload;
   end;
 
 var
@@ -29,51 +22,26 @@ implementation
 uses
   UnitPasTools;
 
-function TULang.SetLang (AFileName : TFileName; ASynEdit : TSynEdit;
-  ASynCSSyn :TSynCSSyn; ASynHTMLSyn :TSynHTMLSyn; ASynJavaSyn : TSynJavaSyn; ASynJSONSyn : TSynJSONSyn;
-    ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn; ASynPythonSyn : TSynPythonSyn;
-  ASynCompletion : TSynCompletion) : string;
+function TULang.SetLang (AFileName : TFileName; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string;
 var
   LFileExt : string;
   LLang : TASETypeLang;
   LLangTxt : string;
 begin
   LFileExt := ExtractFileExt(AFileName);
-  if Pos(LFileExt,ASynCSSyn.DefaultFilter) >= 1 then LLang := aseLangCSharp
-    else if Pos(LFileExt,ASynHTMLSyn.DefaultFilter) >= 1 then LLang := aseLangHTML
-    else if Pos(LFileExt,ASynJavaSyn.DefaultFilter) >= 1 then LLang := aseLangJava
-    else if Pos(LFileExt,ASynJSONSyn.DefaultFilter) >= 1 then LLang := aseLangJSON
-    else if Pos(LFileExt,ASynPasSyn.DefaultFilter) >= 1 then LLang := aseLangPas
-    else if Pos(LFileExt,ASynPHPSyn.DefaultFilter) >= 1 then LLang := aseLangPHP
-    else if Pos(LFileExt,ASynPythonSyn.DefaultFilter) >= 1 then LLang := aseLangPython
-  ;
-  LLangTxt := Self.SetLang(LLang,ASynEdit,
-    ASynCSSyn,ASynHTMLSyn,ASynJavaSyn,ASynJSONSyn,ASynPasSyn,ASynPHPSyn,ASynPythonSyn,
-    ASynCompletion
-  );
+  LLang := VUVar.vSynHighlighter.fcGetLang(LFileExt);
+  LLangTxt := Self.SetLang(LLang,ASynEdit,ASynCompletion);
   Result := LLangTxt;
 end;
 
-function TULang.SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit;
-    ASynCSSyn : TSynCSSyn; ASynHTMLSyn : TSynHTMLSyn; ASynJavaSyn : TSynJavaSyn; ASynJSONSyn : TSynJSONSyn;
-      ASynPasSyn : TSynPasSyn; ASynPHPSyn : TSynPHPSyn; ASynPythonSyn : TSynPythonSyn;
-    ASynCompletion : TSynCompletion
-  ) : string;
+function TULang.SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string;
 var
   LLangTxt : string;
 begin
   ASynCompletion.Editor := ASynEdit;
   ASynCompletion.ItemList := VUTools.FcStringExplode(VUVar.vReservedWords[Ord(ALang)],'|');
   LLangTxt := VUVar.vASETypeLang[Ord(ALang)];
-  case ALang of
-    aseLangCSharp : ASynEdit.Highlighter := ASynCSSyn;
-    aseLangHTML   : ASynEdit.Highlighter := ASynHTMLSyn;
-    aseLangJava   : ASynEdit.Highlighter := ASynJavaSyn;
-    aseLangJSON   : ASynEdit.Highlighter := ASynJSONSyn;
-    aseLangPas    : ASynEdit.Highlighter := ASynPasSyn;
-    aseLangPHP    : ASynEdit.Highlighter := ASynPHPSyn;
-    aseLangPython : ASynEdit.Highlighter := ASynPythonSyn;
-  end;
+  ASynEdit.Highlighter := VUVar.vSynHighlighter.fcGetHighlighter(ALang);
   Result := LLangTxt;
 end;
 
