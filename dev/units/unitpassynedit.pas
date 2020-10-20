ad358dcb20bade1dda1efb547de38947c11b9454
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-20 16:14:41 +0700

    Update unitpassynedit.pas

--- dev/units/unitpassynedit.pas
+++ dev/units/unitpassynedit.pas
@@ -11,6 +11,8 @@ type
   TUSynEdit = class
     constructor Create;
     procedure Update (ASynEdit : TSynEdit; AHighlighter : TSynCustomHighlighter);
+    procedure Open (ASynEdit : TSynEdit; AFileName : TFileName);
+    procedure Save (ASynEdit : TSynEdit; AFileName : TFileName);
   end;
 
 implementation
@@ -23,6 +25,16 @@ end;
 procedure TUSynEdit.Update (ASynEdit : TSynEdit; AHighlighter : TSynCustomHighlighter);
 begin
   ASynEdit.Highlighter := AHighlighter;
+end;
+
+procedure TUSynEdit.Open (ASynEdit : TSynEdit; AFileName : TFileName);
+begin
+  ASynEdit.Lines.LoadFromFile(AFileName);
+end;
+
+procedure TUSynEdit.Save (ASynEdit : TSynEdit; AFileName : TFileName);
+begin
+  ASynEdit.Lines.SaveToFile(AFileName);
 end;
 
 end.
