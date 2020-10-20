8a0ab3fe158da5817f08d2660a8869a7570ae0f2
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-20 16:27:35 +0700

    Update UnitPasLang.pas

--- source/units/UnitPasLang.pas
+++ source/units/UnitPasLang.pas
@@ -5,10 +5,12 @@ unit UnitPasLang;
 interface
 
 uses
-  Classes, SysUtils, SynEdit, SynCompletion, UnitPasVar, UnitPasSynHighlighter;
+  Classes, SysUtils, SynEdit, SynCompletion, UnitPasSynHighlighter;
 
 type
   TULang = object
+    function GetLang (AFileExt : string) : TASETypeLang;
+    function GetLangTxt (AASETypeLang : TASETypeLang) : string;
     function SetLang (AFileName : TFileName; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string; overload;
     function SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string; overload;
   end;
@@ -19,7 +21,17 @@ var
 implementation
 
 uses
-  UnitPasTools;
+  UnitPasVar, UnitPasTools;
+
+function TULang.GetLang (AFileExt : string) : TASETypeLang;
+begin
+  Result := VUVar.vSynHighlighter.fcGetLang(AFileExt);
+end;
+
+function TULang.GetLangTxt (AASETypeLang : TASETypeLang) : string;
+begin
+  Result := VUVar.vASETypeLang[Ord(AASETypeLang)];
+end;
 
 function TULang.SetLang (AFileName : TFileName; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string;
 var
