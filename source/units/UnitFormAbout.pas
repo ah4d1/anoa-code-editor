615ede15ccde42d61b2186a61ac89569f5e23dd8
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-20 10:45:50 +0700

    Create UnitFormAbout.pas

---
+++ source/units/UnitFormAbout.pas
@@ -0,0 +1,41 @@
+unit UnitFormAbout;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  SynEdit;
+
+type
+
+  { TFormAbout }
+
+  TFormAbout = class(TForm)
+    Image1: TImage;
+    PanelLeft: TPanel;
+    SynEdit1: TSynEdit;
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+  private
+
+  public
+
+  end;
+
+var
+  FormAbout: TFormAbout;
+
+implementation
+
+{$R *.lfm}
+
+{ TFormAbout }
+
+procedure TFormAbout.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  Self.Release;
+end;
+
+end.
+
