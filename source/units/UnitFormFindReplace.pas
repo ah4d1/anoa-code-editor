2b45b9901bb206b4fd13be446804746ba9080ca3
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-20 10:45:57 +0700

    Create UnitFormFindReplace.pas

---
+++ source/units/UnitFormFindReplace.pas
@@ -0,0 +1,62 @@
+unit UnitFormFindReplace;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, StdCtrls, SynEditTypes;
+
+type
+
+  { TFormFindReplace }
+
+  TFormFindReplace = class(TForm)
+    ButtonReplaceAll: TButton;
+    ComboBoxDirection: TComboBox;
+    EditFind: TEdit;
+    EditReplaceWith: TEdit;
+    GroupBoxOptions: TGroupBox;
+    LabelFind: TLabel;
+    LabelReplaceWith: TLabel;
+    LabelDirection: TLabel;
+    procedure ButtonReplaceAllClick(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+  private
+
+  public
+
+  end;
+
+var
+  FormFindReplace: TFormFindReplace;
+
+implementation
+
+{$R *.lfm}
+
+{ TFormFindReplace }
+
+uses
+  UnitFormMain;
+
+procedure TFormFindReplace.ButtonReplaceAllClick(Sender: TObject);
+begin
+  FormMain.SynEditMain.SearchReplace(Self.EditFind.Text,Self.EditReplaceWith.Text,[ssoReplaceAll]);;
+end;
+
+procedure TFormFindReplace.FormActivate(Sender: TObject);
+begin
+  Self.AlphaBlend := False;
+  Self.AlphaBlendValue := 255;
+end;
+
+procedure TFormFindReplace.FormClose(Sender: TObject;
+  var CloseAction: TCloseAction);
+begin
+  Self.Release;
+end;
+
+end.
+
