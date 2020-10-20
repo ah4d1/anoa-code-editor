f0c2acea0d3f7557f8b29df36c57c653a3fa6385
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-21 06:56:14 +0700

    Update UnitPasLang.pas

--- dev/units/UnitPasLang.pas
+++ dev/units/UnitPasLang.pas
@@ -5,14 +5,12 @@ unit UnitPasLang;
 interface
 
 uses
-  Classes, SysUtils, SynEdit, SynCompletion, UnitPasSynHighlighter;
+  Classes, SysUtils, UnitPasSynHighlighter;
 
 type
   TULang = object
     function GetLang (AFileExt : string) : TASETypeLang;
     function GetLangTxt (AASETypeLang : TASETypeLang) : string;
-    function SetLang (AFileName : TFileName; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string; overload;
-    function SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string; overload;
   end;
 
 var
@@ -21,7 +19,7 @@ var
 implementation
 
 uses
-  UnitPasVar, UnitPasTools;
+  UnitPasVar;
 
 function TULang.GetLang (AFileExt : string) : TASETypeLang;
 begin
@@ -31,29 +29,6 @@ end;
 function TULang.GetLangTxt (AASETypeLang : TASETypeLang) : string;
 begin
   Result := VUVar.vASETypeLang[Ord(AASETypeLang)];
-end;
-
-function TULang.SetLang (AFileName : TFileName; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string;
-var
-  LFileExt : string;
-  LLang : TASETypeLang;
-  LLangTxt : string;
-begin
-  LFileExt := ExtractFileExt(AFileName);
-  LLang := VUVar.vSynHighlighter.fcGetLang(LFileExt);
-  LLangTxt := Self.SetLang(LLang,ASynEdit,ASynCompletion);
-  Result := LLangTxt;
-end;
-
-function TULang.SetLang (ALang : TASETypeLang; ASynEdit : TSynEdit; ASynCompletion : TSynCompletion) : string;
-var
-  LLangTxt : string;
-begin
-  ASynCompletion.Editor := ASynEdit;
-  ASynCompletion.ItemList := VUTools.FcStringExplode(VUVar.vReservedWords.vReservedWords[Ord(ALang)],'|');
-  ASynEdit.Highlighter := VUVar.vSynHighlighter.fcGetHighlighter(ALang);
-  LLangTxt := VUVar.vASETypeLang[Ord(ALang)];
-  Result := LLangTxt;
 end;
 
 end.
