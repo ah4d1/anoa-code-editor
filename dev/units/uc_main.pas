5118025ad9fc2051ce28d895ea7c27d038a375f0
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-22 12:06:04 +0700

    Update uc_main.pas

--- dev/units/uc_main.pas
+++ dev/units/uc_main.pas
@@ -5,7 +5,8 @@ unit uc_main;
 interface
 
 uses
-  Classes, SysUtils, Controls, Menus, SynEditHighlighter, up_synhighlighter, uc_pagecontrol, uc_statusbar;
+  Classes, SysUtils, Controls, Menus, SynEditHighlighter, up_var, up_currentdata,
+  uc_pagecontrol, uc_statusbar, Dialogs;
 
 type
   tucMain = class(TComponent)
@@ -18,21 +19,27 @@ type
     constructor Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu); overload;
     procedure fcAddTab; overload;
     procedure fcAddTab (AFileName : TFileName); overload;
-    procedure fcUpdate (ALang : TASETypeLang; AHighlighter : TSynCustomHighlighter);
+    procedure fcUpdate (ACurrentData : tupCurrentData);
+    procedure fcUndo;
+    procedure fcRedo;
+    procedure fcSetFontSize (ASize : Byte);
+    procedure fcSave (AFileName : TFileName);
+    procedure fcShowCompletion;
+    procedure fcSwitchEditorColor;
   end;
+
+var
+  vucMain : tucMain;
 
 implementation
 
-uses
-  up_var;
-
-constructor tucMain.Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu);
+constructor tucMain.Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu); overload;
 begin
-  Self.vPageControl := tucPageControl.Create(AOwner);
-  Self.vPageControl.Parent := AOwner;
-  Self.vPageControl.fcInit(AImageList,APopupMenu);
   Self.vStatusBar := tucStatusBar.Create(AOwner);
   Self.vStatusBar.Parent := AOwner;
+  Self.vPageControl := tucPageControl.Create(AOwner);
+  Self.vPageControl.Parent := AOwner;
+  Self.vPageControl.fcInit(AImageList,APopupMenu,Self.vStatusBar);
 end;
 
 procedure tucMain.fcAddTab;
@@ -47,10 +54,40 @@ begin
   Self.vPageControl.fcAddTabThenOpen(AFileName,vupVar.vImageIndexNormalFile);
 end;
 
-procedure tucMain.fcUpdate (ALang : TASETypeLang; AHighlighter : TSynCustomHighlighter);
+procedure tucMain.fcUpdate (ACurrentData : tupCurrentData);
 begin
-  Self.vPageControl.fcUpdate(ALang,AHighlighter);
-  Self.vStatusBar.fcUpdate(ALang);
+  Self.vPageControl.fcUpdate(ACurrentData);
+  Self.vStatusBar.fcUpdate(ACurrentData);
+end;
+
+procedure tucMain.fcUndo;
+begin
+  Self.vPageControl.fcUndo;
+end;
+
+procedure tucMain.fcRedo;
+begin
+  Self.vPageControl.fcRedo;
+end;
+
+procedure tucMain.fcSetFontSize (ASize : Byte);
+begin
+  Self.vPageControl.fcSetFontSize(ASize);
+end;
+
+procedure tucMain.fcSave (AFileName : TFileName);
+begin
+  Self.vPageControl.fcSave(AFileName);
+end;
+
+procedure tucMain.fcShowCompletion;
+begin
+  Self.vPageControl.fcShowCompletion;
+end;
+
+procedure tucMain.fcSwitchEditorColor;
+begin
+  Self.vPageControl.fcSwitchEditorColor;
 end;
 
 end.
