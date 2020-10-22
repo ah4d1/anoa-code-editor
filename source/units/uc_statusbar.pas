c6a0febcca75dd4a09e1660d53d7ee6758a5e597
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-22 12:13:50 +0700

    Create uc_statusbar.pas

---
+++ source/units/uc_statusbar.pas
@@ -0,0 +1,48 @@
+unit uc_statusbar;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, ComCtrls, up_currentdata, up_synhighlighter, Dialogs;
+
+type
+  tucStatusBar = class(TStatusBar)
+  public
+    constructor Create (AOwner : TComponent); override;
+    procedure fcUpdate (ACurrentData : tupCurrentData); overload;
+  private
+    procedure fcUpdate (ALangTxt : string; AFileName : TFileName); overload;
+  end;
+
+implementation
+
+constructor tucStatusBar.Create (AOwner : TComponent);
+begin
+  inherited Create(AOwner);
+  with Self.Panels.Add do
+  begin
+    Alignment := taCenter;
+    Width := 100;
+  end;
+  with Self.Panels.Add do
+  begin
+    Alignment := taLeftJustify;
+  end;
+  Self.SimplePanel := False;
+end;
+
+procedure tucStatusBar.fcUpdate (ACurrentData : tupCurrentData);
+begin
+  Self.fcUpdate(ACurrentData.vLangTxt,ACurrentData.vFileName);
+end;
+
+procedure tucStatusBar.fcUpdate (ALangTxt : string; AFileName : TFileName);
+begin
+  Self.Panels[0].Text := ALangTxt;
+  Self.Panels[1].Text := AFileName;
+end;
+
+end.
+
