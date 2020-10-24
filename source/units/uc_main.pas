7ef71994f15a41c84a03296ed027b6fefed09845
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-24 21:00:50 +0700

    Update uc_main.pas

--- source/units/uc_main.pas
+++ source/units/uc_main.pas
@@ -5,24 +5,34 @@ unit uc_main;
 interface
 
 uses
-  Classes, SysUtils, Controls, Menus, SynEditHighlighter, up_var, up_currentdata,
+  Classes, SysUtils, Controls, Menus, Spin, SynEditHighlighter, up_var, up_currentdata,
   uc_pagecontrol, uc_statusbar, Dialogs;
 
 type
   tucMain = class(TComponent)
   private
+    FSpinEdit : TSpinEdit;
     FPageControl : tucPageControl;
     FStatusBar : tucStatusBar;
+    FSaveDialog : TSaveDialog;
   public
+    property vSpinEdit : TSpinEdit read FSpinEdit write FSpinEdit;
     property vPageControl : tucPageControl read FPageControl write FPageControl;
     property vStatusBar : tucStatusBar read FStatusBar write FStatusBar;
-    constructor Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu); overload;
+    property vSaveDialog : TSaveDialog read FSaveDialog write FSaveDialog;
+    constructor Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu;
+      ASpinEdit : TSpinEdit; ASaveDialog : TSaveDialog); overload;
     procedure fcAddTab; overload;
-    procedure fcAddTab (AFileName : TFileName); overload;
+    procedure fcAddTab (ACurrentData : tupCurrentData); overload;
+    procedure fcCloseTab (ACurrentData : tupCurrentData);
     procedure fcUpdate (ACurrentData : tupCurrentData);
+    procedure fcUpdateFontSize;
     procedure fcUndo;
     procedure fcRedo;
-    procedure fcSetFontSize (ASize : Byte);
+    procedure fcCopy;
+    procedure fcCut;
+    procedure fcPaste;
+    procedure fcSelectAll;
     procedure fcSave (AFileName : TFileName);
     procedure fcShowCompletion;
     procedure fcSwitchEditorColor;
@@ -33,13 +43,16 @@ var
 
 implementation
 
-constructor tucMain.Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu); overload;
+constructor tucMain.Create (AOwner : TWinControl; AImageList : TImageList; APopupMenu : TPopupMenu;
+  ASpinEdit : TSpinEdit; ASaveDialog : TSaveDialog); overload;
 begin
+  Self.vSpinEdit := ASpinEdit;
+  Self.vSaveDialog := ASaveDialog;
   Self.vStatusBar := tucStatusBar.Create(AOwner);
   Self.vStatusBar.Parent := AOwner;
   Self.vPageControl := tucPageControl.Create(AOwner);
   Self.vPageControl.Parent := AOwner;
-  Self.vPageControl.fcInit(AImageList,APopupMenu,Self.vStatusBar);
+  Self.vPageControl.fcInit(AImageList,APopupMenu,Self.vSpinEdit,Self.vStatusBar,Self.vSaveDialog);
 end;
 
 procedure tucMain.fcAddTab;
@@ -48,16 +61,29 @@ begin
   Self.vPageControl.fcAddTab(vupVar.vTabPrefix + IntToStr(vupVar.vTabNo),vupVar.vImageIndexNormalFile);
 end;
 
-procedure tucMain.fcAddTab (AFileName : TFileName);
+procedure tucMain.fcAddTab (ACurrentData : tupCurrentData);
 begin
   vupVar.vTabNo := vupVar.vTabNo + 1;
-  Self.vPageControl.fcAddTabThenOpen(AFileName,vupVar.vImageIndexNormalFile);
+  Self.vPageControl.fcAddTabThenOpen(ACurrentData,vupVar.vImageIndexNormalFile);
+  Self.vStatusBar.fcUpdate(ACurrentData);
+end;
+
+procedure tucMain.fcCloseTab (ACurrentData : tupCurrentData);
+begin
+  Self.vPageControl.fcCloseTab(ACurrentData,vupVar.vTabPrefix + IntToStr(vupVar.vTabNo),
+    vupVar.vImageIndexNormalFile
+  );
 end;
 
 procedure tucMain.fcUpdate (ACurrentData : tupCurrentData);
 begin
   Self.vPageControl.fcUpdate(ACurrentData);
   Self.vStatusBar.fcUpdate(ACurrentData);
+end;
+
+procedure tucMain.fcUpdateFontSize;
+begin
+  Self.vPageControl.fcUpdateFontSize;
 end;
 
 procedure tucMain.fcUndo;
@@ -70,9 +96,24 @@ begin
   Self.vPageControl.fcRedo;
 end;
 
-procedure tucMain.fcSetFontSize (ASize : Byte);
+procedure tucMain.fcCopy;
 begin
-  Self.vPageControl.fcSetFontSize(ASize);
+  Self.vPageControl.fcCopy;
+end;
+
+procedure tucMain.fcCut;
+begin
+  Self.vPageControl.fcCut;
+end;
+
+procedure tucMain.fcPaste;
+begin
+  Self.vPageControl.fcPaste;
+end;
+
+procedure tucMain.fcSelectAll;
+begin
+  Self.vPageControl.fcSelectAll;
 end;
 
 procedure tucMain.fcSave (AFileName : TFileName);
