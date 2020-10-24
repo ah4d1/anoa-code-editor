1793e1a6cb3e9cdd7065989211870770a5749428
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-24 21:00:40 +0700

    Update uc_tabsheet.pas

--- source/units/uc_tabsheet.pas
+++ source/units/uc_tabsheet.pas
@@ -5,10 +5,11 @@ unit uc_tabsheet;
 interface
 
 uses
-  Classes, SysUtils, ComCtrls, SynEditHighlighter, up_var, up_currentdata, uc_synedit,
+  Classes, SysUtils, ComCtrls, SynEditHighlighter, Spin, up_var, up_currentdata, uc_synedit,
   uc_syncompletion, up_synhighlighter, Dialogs;
 
 type
+  taseTextStatus = (aseTextStatusNormal,aseTextStatusModified);
   tucTabSheet = class(TTabSheet)
   private
     FLang : taseLang;
@@ -16,20 +17,25 @@ type
     FFileName : TFileName;
     FSynEdit : tucSynEdit;
     FSynCompletion : tucSynCompletion;
+    FTextStatus : taseTextStatus;
   public
     property vLang : taseLang read FLang write FLang;
     property vLangTxt : string read FLangTxt write FLangTxt;
     property vFileName : TFileName read FFileName write FFileName;
     property vSynEdit : tucSynEdit read FSynEdit write FSynEdit;
     property vSynCompletion : tucSynCompletion read FSynCompletion write FSynCompletion;
+    property vTextStatus : taseTextStatus read FTextStatus write FTextStatus;
     constructor Create (AOwner : TComponent); override;
     procedure fcInit;
-    procedure fcOpen (AFileName : TFileName);
+    procedure fcOpen (ACurrentData : tupCurrentData);
     procedure fcSave (AFileName : TFileName);
     procedure fcUpdate (ACurrentData : tupCurrentData);
     procedure fcUndo;
     procedure fcRedo;
-    procedure fcSetFontSize (ASize : Byte);
+    procedure fcCopy;
+    procedure fcCut;
+    procedure fcPaste;
+    procedure fcSelectAll;
     procedure fcSetCurrentData;
     procedure fcShowCompletion;
     procedure fcSwitchEditorColor;
@@ -51,26 +57,29 @@ begin
   Self.vLang := aseLangNone;
   Self.vLangTxt := '';
   Self.vFileName := '';
+  Self.vTextStatus := aseTextStatusNormal;
 end;
 
-procedure tucTabSheet.fcOpen (AFileName : TFileName);
+procedure tucTabSheet.fcOpen (ACurrentData : tupCurrentData);
 begin
-  Self.vSynEdit.fcOpen(AFileName);
+  Self.vSynEdit.fcOpen(ACurrentData.vFileName);
+  Self.fcUpdate(ACurrentData);
 end;
 
 procedure tucTabSheet.fcSave (AFileName : TFileName);
 begin
   Self.Caption := ExtractFileName(AFileName);
   Self.ImageIndex := vupVar.vImageIndexNormalFile;
+  Self.vTextStatus := aseTextStatusNormal;
   Self.vSynEdit.fcSave(AFileName);
 end;
 
 procedure tucTabSheet.fcUpdate (ACurrentData : tupCurrentData);
 begin
-  Self.vSynEdit.fcUpdate(vupCurrentData.vHighlighter);
-  Self.vSynCompletion.fcUpdate(vupCurrentData.vLang);
-  Self.vLangTxt := vupCurrentData.vLangTxt;
-  Self.vFileName := vupCurrentData.vFileName;
+  Self.vSynEdit.fcUpdate(ACurrentData);
+  Self.vSynCompletion.fcUpdate(ACurrentData.vLang);
+  Self.vLangTxt := ACurrentData.vLangTxt;
+  Self.vFileName := ACurrentData.vFileName;
 end;
 
 procedure tucTabSheet.fcUndo;
@@ -83,9 +92,24 @@ begin
   Self.vSynEdit.fcRedo;
 end;
 
-procedure tucTabSheet.fcSetFontSize (ASize : Byte);
+procedure tucTabSheet.fcCopy;
 begin
-  Self.vSynEdit.fcSetFontSize(ASize);
+  Self.vSynEdit.fcCopy;
+end;
+
+procedure tucTabSheet.fcCut;
+begin
+  Self.vSynEdit.fcCut;
+end;
+
+procedure tucTabSheet.fcPaste;
+begin
+  Self.vSynEdit.fcPaste;
+end;
+
+procedure tucTabSheet.fcSelectAll;
+begin
+  Self.vSynEdit.fcSelectAll;
 end;
 
 procedure tucTabSheet.fcSetCurrentData;
