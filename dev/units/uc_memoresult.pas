d26d6bf30bebe7461578682dbbedc651fa5435eb
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-28 13:35:14 +0700

    Update uc_memoresult.pas

--- dev/units/uc_memoresult.pas
+++ dev/units/uc_memoresult.pas
@@ -20,6 +20,8 @@ begin
   Self.Align := alBottom;
   Self.Color := clBlack;
   Self.Font.Color := clWhite;
+  Self.Font.Name := 'Courier New';
+  Self.Font.Size := 9;
   Self.Height := 0;
   Self.ReadOnly := True;
   Self.ScrollBars := ssAutoBoth;
