193f098757ea83fbffebf3319c438dc0e3ee64e4
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-29 10:21:02 +0700

    Update uc_tabsheet.pas

--- source/units/uc_tabsheet.pas
+++ source/units/uc_tabsheet.pas
@@ -5,8 +5,9 @@ unit uc_tabsheet;
 interface
 
 uses
-  Classes, SysUtils, ComCtrls, SynEditHighlighter, Spin, up_var, up_currentdata, uc_synedit,
-  uc_syncompletion, up_synhighlighter, Dialogs, SynEditTypes;
+  Classes, SysUtils, ComCtrls, SynEditHighlighter, up_var, up_currentdata, uc_synedit,
+  uc_syncompletion, up_synhighlighter, Dialogs, SynEditTypes, Menus, uc_memoresult, ExtCtrls,
+  Controls;
 
 type
   taseTextStatus = (aseTextStatusNormal,aseTextStatusModified);
@@ -16,6 +17,8 @@ type
     FLangTxt : string;
     FFileName : TFileName;
     FSynEdit : tucSynEdit;
+    FSplitter : TSplitter;
+    FMemoResult : tucMemoResult;
     FSynCompletion : tucSynCompletion;
     FTextStatus : taseTextStatus;
   public
@@ -23,13 +26,16 @@ type
     property vLangTxt : string read FLangTxt write FLangTxt;
     property vFileName : TFileName read FFileName write FFileName;
     property vSynEdit : tucSynEdit read FSynEdit write FSynEdit;
+    property vSplitter : TSplitter read FSplitter write FSplitter;
+    property vMemoResult : tucMemoResult read FMemoResult write FMemoResult;
     property vSynCompletion : tucSynCompletion read FSynCompletion write FSynCompletion;
     property vTextStatus : taseTextStatus read FTextStatus write FTextStatus;
     constructor Create (AOwner : TComponent); override;
-    procedure fcInit;
+    procedure fcInit (APopupMenu : TPopupMenu);
     procedure fcOpen (ACurrentData : tupCurrentData);
     procedure fcSave (AFileName : TFileName);
-    procedure fcUpdate (ACurrentData : tupCurrentData);
+    procedure fcUpdate (AVar : tupVar); overload;
+    procedure fcUpdate (ACurrentData : tupCurrentData); overload;
     procedure fcUndo;
     procedure fcRedo;
     procedure fcCopy;
@@ -39,26 +45,36 @@ type
     procedure fcSetCurrentData;
     procedure fcShowCompletion;
     procedure fcReplace (AOldPattern,ANewPattern : string; ASynSearchOptions : TSynSearchOptions);
-    procedure fcSwitchEditorColor;
+    procedure fcFindNext;
+    procedure fcRunCommand;
   end;
 
 implementation
 
+uses
+  up_runcommand, ac_dialog;
+
 constructor tucTabSheet.Create (AOwner : TComponent);
 begin
   inherited Create(AOwner);
+  Self.vMemoResult := tucMemoResult.Create(AOwner);
+  Self.vMemoResult.Parent := Self;
+  Self.vSplitter := TSplitter.Create(AOwner);
+  Self.vSplitter.Parent := Self;
+  Self.vSplitter.Align := alBottom;
   Self.vSynEdit := tucSynEdit.Create(AOwner);
   Self.vSynEdit.Parent := Self;
   Self.vSynCompletion := tucSynCompletion.Create(AOwner);
   Self.vSynCompletion.Editor := Self.vSynEdit;
 end;
 
-procedure tucTabSheet.fcInit;
+procedure tucTabSheet.fcInit (APopupMenu : TPopupMenu);
 begin
   Self.vLang := aseLangNone;
   Self.vLangTxt := '';
   Self.vFileName := '';
   Self.vTextStatus := aseTextStatusNormal;
+  Self.vSynEdit.fcInit(APopupMenu);
 end;
 
 procedure tucTabSheet.fcOpen (ACurrentData : tupCurrentData);
@@ -75,10 +91,16 @@ begin
   Self.vSynEdit.fcSave(AFileName);
 end;
 
+procedure tucTabSheet.fcUpdate (AVar : tupVar);
+begin
+  Self.vSynEdit.fcUpdate(AVar);
+end;
+
 procedure tucTabSheet.fcUpdate (ACurrentData : tupCurrentData);
 begin
   Self.vSynEdit.fcUpdate(ACurrentData);
   Self.vSynCompletion.fcUpdate(ACurrentData.vLang);
+  Self.vLang := ACurrentData.vLang;
   Self.vLangTxt := ACurrentData.vLangTxt;
   Self.vFileName := ACurrentData.vFileName;
 end;
@@ -115,8 +137,7 @@ end;
 
 procedure tucTabSheet.fcSetCurrentData;
 begin
-  vupCurrentData.vLangTxt := Self.vLangTxt;
-  vupCurrentData.vFileName := Self.vFileName;
+  vupCurrentData.fcUpdate(Self.vLang,Self.vFileName);
 end;
 
 procedure tucTabSheet.fcShowCompletion;
@@ -129,9 +150,17 @@ begin
   Self.vSynEdit.fcReplace(AOldPattern,ANewPattern,ASynSearchOptions);
 end;
 
-procedure tucTabSheet.fcSwitchEditorColor;
+procedure tucTabSheet.fcFindNext;
 begin
-  Self.vSynEdit.fcSwitchColor;
+  Self.vSynEdit.fcFindNext;
+end;
+
+procedure tucTabSheet.fcRunCommand;
+begin
+  case Self.vTextStatus of
+    aseTextStatusNormal : vupRunCommand.fcRun(Self.vLang,Self.vFileName,Self.vSynEdit,Self.vMemoResult,Self);
+    aseTextStatusModified : vacDialog.fcInfo('Save your work before run.');
+  end;
 end;
 
 end.
