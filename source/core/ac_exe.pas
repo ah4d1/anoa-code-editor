4c6ccca3c94ca42c4b27eafed48acb93b4f2ebae
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-10-29 10:20:36 +0700

    Create ac_exe.pas

---
+++ source/core/ac_exe.pas
@@ -0,0 +1,42 @@
+unit ac_exe;
+
+{**************************************************************
+*  A Part of Anoa-Core                                        *
+*  See https://github.com/ah4d1/anoa-core                     *
+**************************************************************}
+
+interface
+
+uses
+  Classes, SysUtils, Process;
+
+type
+  tacExe = object
+  public
+    function fcRun (AExeName : string; ACommand : string) : TStringList;
+  end;
+
+var
+  vacExe : tacExe;
+
+implementation
+
+uses
+  ac_string;
+
+{One command & No Console}
+{Example : fcRun('python','C:/myfile.py')}
+function tacExe.fcRun (AExeName : string; ACommand : string) : TStringList;
+var
+  LOutputString : string;
+  LOutputStrings : TStringList;
+begin
+  {poNoConsole : no cmd window}
+  {poStderrToOutPut : get error output}
+  RunCommand(AExeName,[ACommand],LOutputString,[poNoConsole,poStderrToOutPut]);
+  LOutputStrings := vacString.fcSplit(LOutputString,#13);
+  Result := LOutputStrings;
+end;
+
+end.
+
