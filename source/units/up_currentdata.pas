46c50d30a1e609857cab27908ef924d08113c272
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-11-02 15:33:00 +0700

    Update up_currentdata.pas

--- source/units/up_currentdata.pas
+++ source/units/up_currentdata.pas
@@ -5,19 +5,19 @@ unit up_currentdata;
 interface
 
 uses
-  Classes, SysUtils, SynEditHighlighter, up_synhighlighter, Dialogs;
+  Classes, SysUtils, SynEditHighlighter, ace_synhighlighter, Dialogs;
 
 type
   tupCurrentData = class
-    vLang : taseLang;
+    vLang : TAceShLang;
     vLangTxt : string;
-    vSynHighlighter : tupSynHighlighter;
+    vSynHighlighter : TAceSynHighlighter;
     vHighlighter : TSynCustomHighlighter;
     vFileName : TFileName;
-    constructor Create (AHighlighter : tupSynHighlighter);
-    procedure fcUpdate (ALang : taseLang); overload;
+    constructor Create (AHighlighter : TAceSynHighlighter);
+    procedure fcUpdate (ALang : TAceShLang); overload;
     procedure fcUpdate (AFileName : TFileName); overload;
-    procedure fcUpdate (ALang : taseLang; AFileName : TFileName); overload;
+    procedure fcUpdate (ALang : TAceShLang; AFileName : TFileName); overload;
   end;
 
 var
@@ -26,18 +26,18 @@ var
 implementation
 
 uses
-  up_lang;
+  up_lang, up_var;
 
-constructor tupCurrentData.Create (AHighlighter : tupSynHighlighter);
+constructor tupCurrentData.Create (AHighlighter : TAceSynHighlighter);
 begin
-  Self.vLang := aseLangNone;
+  Self.vLang := aceShLangNone;
   Self.vLangTxt := '';
   Self.vSynHighlighter := AHighlighter;
   Self.vHighlighter := AHighlighter.vNone;
   Self.vFileName := '';
 end;
 
-procedure tupCurrentData.fcUpdate (ALang : taseLang);
+procedure tupCurrentData.fcUpdate (ALang : TAceShLang);
 begin
   Self.fcUpdate(ALang,Self.vFileName);
 end;
@@ -48,11 +48,13 @@ begin
   Self.fcUpdate(Self.vLang,AFileName);
 end;
 
-procedure tupCurrentData.fcUpdate (ALang : taseLang; AFileName : TFileName);
+procedure tupCurrentData.fcUpdate (ALang : TAceShLang; AFileName : TFileName);
 begin
   Self.vLang := ALang;
   Self.vLangTxt := vupLang.fcGetLangTxt(Self.vLang);
-  Self.vHighlighter := Self.vSynHighlighter.fcGetHighlighter(Self.vLang);
+  vupVar.vSynHighlighter.vLang := Self.vLang;
+  Self.vHighlighter := vupVar.vSynHighlighter.vHighlighter;
+  // Self.vHighlighter := Self.vSynHighlighter.fcGetHighlighter(Self.vLang);
   Self.vFileName := AFileName;
 end;
 
