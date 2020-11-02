6aeb7c34fb9fe11fedcd8bbb4b51cbbd69420b8c
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-11-02 15:33:15 +0700

    Create up_theme.pas

---
+++ source/units/up_theme.pas
@@ -0,0 +1,81 @@
+unit up_theme;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SynEditHighlighter, Graphics, up_var, ace_synedit;
+
+type
+  tupTheme = object
+    vSynEditColor : TColor;
+    vSynEditFontColor : TColor;
+    vGutterColor : TColor;
+    vGutterMarkupColor : TColor;
+    vLineHighlightColor : TColor;
+    vCommentAttriColor : TColor;
+    vKeyAttriColor : TColor;
+    procedure fcSetThemeColor (AVar : tupVar);
+    procedure fcSetVarColor (AVar : tupVar);
+    procedure fcSetAttriColor (ACommentAttriColor,AKeyAttriColor : TColor);
+  private
+    procedure fcSetColorDetail (ALang : TSynCustomHighlighter; ACommentAttriColor,AKeyAttriColor : TColor);
+    procedure fcSetColorDetail (ALang : TSynCustomHighlighter; AKeyAttriColor : TColor);
+  end;
+
+var
+  vupTheme : tupTheme;
+
+implementation
+
+uses
+  ac_color;
+
+procedure tupTheme.fcSetThemeColor (AVar : tupVar);
+begin
+  Self.fcSetVarColor(AVar);
+  Self.fcSetAttriColor(Self.vCommentAttriColor,Self.vKeyAttriColor);
+end;
+
+procedure tupTheme.fcSetVarColor (AVar : tupVar);
+begin
+  if vupVar.vCurrentTheme = aceSeThemeNormal then
+  begin
+    Self.vCommentAttriColor := AVar.vSynHighlighter.vCommentAttriColor;
+    Self.vKeyAttriColor := AVar.vSynHighlighter.vKeyAttriColor;
+  end
+  else if vupVar.vCurrentTheme = aceSeThemeDark then
+  begin
+    Self.vCommentAttriColor := vacColor.fcInvert(AVar.vSynHighlighter.vCommentAttriColor);
+    Self.vKeyAttriColor := vacColor.fcInvert(AVar.vSynHighlighter.vKeyAttriColor);
+  end;
+end;
+
+procedure tupTheme.fcSetAttriColor (ACommentAttriColor,AKeyAttriColor : TColor);
+begin
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vCobol,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vCSharp,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vCSS,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vHTML,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vJava,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vJSON,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vPascal,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vPHP,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vPython,ACommentAttriColor,AKeyAttriColor);
+  Self.fcSetColorDetail(vupVar.vSynHighlighter.vSQL,ACommentAttriColor,AKeyAttriColor);
+end;
+
+procedure tupTheme.fcSetColorDetail (ALang : TSynCustomHighlighter; ACommentAttriColor,AKeyAttriColor : TColor);
+begin
+  ALang.CommentAttribute.Foreground := ACommentAttriColor;
+  ALang.KeywordAttribute.Foreground := AKeyAttriColor;
+end;
+
+procedure tupTheme.fcSetColorDetail (ALang : TSynCustomHighlighter; AKeyAttriColor : TColor);
+begin
+  ALang.KeywordAttribute.Foreground := AKeyAttriColor;
+end;
+
+end.
+
