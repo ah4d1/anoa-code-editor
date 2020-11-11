1a1b2d70eddf5ceaa0cf954256dc9a7b5e184ec1
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-11-11 18:38:20 +0700

    Update up_reservewords.pas

--- dev/units/up_reservewords.pas
+++ dev/units/up_reservewords.pas
@@ -10,9 +10,6 @@ uses
 type
   tupReserveWords = class
     constructor Create (ASynHighlighter : TAceSynHighlighter);
-  private
-    procedure fcResWordToArr (ASynHighlighter : TAceSynHighlighter);
-    function fcResWord (ALangTxt : string) : WideString;
   end;
 
 var
@@ -21,31 +18,11 @@ var
 implementation
 
 uses
-  up_var, ac_filedir, ac_string, ac_stringlist;
+  up_var;
 
 constructor tupReserveWords.Create (ASynHighlighter : TAceSynHighlighter);
 begin
-  Self.fcResWordToArr(ASynHighlighter);
-end;
-
-procedure tupReserveWords.fcResWordToArr (ASynHighlighter : TAceSynHighlighter);
-var
-  i : Integer;
-  LLangs : TStringList;
-begin
-  LLangs := ASynHighlighter.vLangs;
-  for i := 0 to LLangs.Count - 1 do
-  begin
-    ASynHighlighter.vReservedWords.Add(string(Self.fcResWord(LLangs[i])));
-  end;
-end;
-
-function tupReserveWords.fcResWord (ALangTxt : string) : WideString;
-var
-  LResWordList : TStringList;
-begin
-  LResWordList := vacFileDir.fcFileToStringList(vupVar.vAppDir + '/addon/resword/' + ALangTxt + '.rw');
-  Result := vacStringList.fcToDelimited(LResWordList,'|');
+  ASynHighlighter.fcReserveWords(vupVar.vAppDir + '/addon/resword/');
 end;
 
 end.
