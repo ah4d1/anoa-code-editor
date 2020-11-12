aabae7916f29695a0ae6738b3534729aaaedff3e
ah4d1 <32898545+ah4d1@users.noreply.github.com>
2020-11-13 05:05:00 +0700

    Update up_reservewords.pas

--- source/units/up_reservewords.pas
+++ source/units/up_reservewords.pas
@@ -1,25 +1,15 @@
 unit up_reservewords;
 
-{
-HOW TO ADD NEW RESERVED WORD :
-1. Add reserve word list *.rw in directory addon/resword
-}
-
 {$mode objfpc}{$H+}
 
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, ace_synhighlighter, Dialogs;
 
 type
   tupReserveWords = class
-    vLangTxt          : WideString;
-    vReservedWords    : array[0..10] of WideString; // always check size of the array
-    constructor Create;
-  private
-    procedure fcResWordToArr (ALang : Widestring);
-    function fcResWord (ALangTxt : string) : WideString;
+    constructor Create (ASynHighlighter : TAceSynHighlighter);
   end;
 
 var
@@ -28,31 +18,11 @@ var
 implementation
 
 uses
-  up_var, ac_filedir, ac_string, ac_stringlist;
+  up_var;
 
-constructor tupReserveWords.Create;
+constructor tupReserveWords.Create (ASynHighlighter : TAceSynHighlighter);
 begin
-  Self.vLangTxt := '|COBOL|C#|CSS|HTML|Java|JSON|Pascal|PHP|Python|SQL';
-  Self.fcResWordToArr('none|cobol|cs|css|html|java|json|pascal|php|python|sql');
-end;
-
-procedure tupReserveWords.fcResWordToArr (ALang : Widestring);
-var
-  i : Integer;
-  LLangs : TStringList;
-begin
-  LLangs := vacString.fcSplit(string(ALang),Char('|'));
-  for i := 0 to LLangs.Count - 1 do
-    Self.vReservedWords[i] := Self.fcResWord(LLangs[i])
-  ;
-end;
-
-function tupReserveWords.fcResWord (ALangTxt : string) : WideString;
-var
-  LResWordList : TStringList;
-begin
-  LResWordList := vacFileDir.fcFileToStringList(vupVar.vAppDir + '/addon/resword/' + ALangTxt + '.rw');
-  Result := vacStringList.fcToDelimited(LResWordList,'|');
+  ASynHighlighter.fcReserveWords(vupVar.vAppDir + '/resword/');
 end;
 
 end.
